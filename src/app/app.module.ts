e3f3a70a53e5965d3f20f57dec68e6ee8de07e8c
Christy Babu <christy.babu@Aufaittechnologies.net>
2018-01-02 14:42:00 +0530

    sass to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// angular metiriat import
+
+//import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule
+   // BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
